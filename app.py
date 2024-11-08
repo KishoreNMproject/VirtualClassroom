027dfa066a16b1a122e5e36bff69020eef17ed71
KishoreNMproject <sproutingkishore2005@gmail.com>
2024-11-08 22:31:50 +0530

    s3 bucket update

--- app.py
+++ app.py
@@ -56,8 +56,8 @@ def login():
 @app.route('/dashboard')
 def dashboard():
     course_urls = [
-        'https://aws-project-virtualclassroom.s3.ap-south-1.amazonaws.com/python_code.pdf',
-        'https://aws-project-virtualclassroom.s3.ap-south-1.amazonaws.com/PYTHON+PROGRAMMING+NOTES.pdf'
+        'https://aws-project-virtual-classroom.s3.us-east-1.amazonaws.com/python_code.pdf',
+        'https://aws-project-virtual-classroom.s3.us-east-1.amazonaws.com/Python+Programming.pdf'
     ]
     
     return render_template('dashboard.html', course_urls=course_urls)
