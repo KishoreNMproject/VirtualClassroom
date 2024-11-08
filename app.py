90043b3ed244ac73651b8783345e9f8ec9322be2
KishoreNMproject <sproutingkishore2005@gmail.com>
2024-11-08 20:27:05 +0530

    updating database details
    database updated so now login and register screens can be tested effectively

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app.secret_key = 'temporary_key'
 # Database connection
 def get_db_connection():
     return mysql.connector.connect(
-        host='database-2.c5eyis2co1ux.ap-south-1.rds.amazonaws.com',
+        host='vc-db.c90o4qqesdgx.us-east-1.rds.amazonaws.com',
         user='admin',
-        password='azam_1234',
-        database='course_app'
+        password='lmUuyP0sUjhbEqvkku5m',
+        database='vc-db'
     )
 
 # Registration Route
