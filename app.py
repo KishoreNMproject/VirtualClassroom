6321577660505e5dc352c0f26e59bdf4c5b30ee0
KishoreNMproject <sproutingkishore2005@gmail.com>
2024-11-11 21:51:25 +0530

    minor security fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import mysql.connector
 import os
 import boto3
 from werkzeug.utils import secure_filename
-from dotenv import load_dotenv
+
 
 
 app = Flask(__name__)
@@ -50,9 +50,6 @@ def register():
     return render_template('register.html')
 
 # AWS S3 configuration
-load_dotenv()
-access_key=os.getenv('AWS_ACCESS_KEY_ID')
-secret_key=os.getenv('AWS_SECRET_ACCESS_KEY')
 s3 = boto3.client('s3', 
                   aws_access_key_id='access_key', 
                   aws_secret_access_key='secret_key',
